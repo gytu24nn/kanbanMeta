41b8a5bc791d09d470b2f3553b114f7c7411289d
Sebastian Kvist <sebastian_kvist@hotmail.com>
2024-10-11 10:54:54 +0200

    Added alert if wrong

--- modules/loginpage.mjs
+++ modules/loginpage.mjs
@@ -3,6 +3,7 @@ export function loginPage() {
     let userName = document.getElementById("userName");
     let password = document.getElementById("password");
     let loginBtn = document.getElementById("loginBtn");
+    let loginFalse = false;
 
     loginBtn.addEventListener ("click", () => {
         let userNameValue = userName.value.toLowerCase();
@@ -17,8 +18,11 @@ export function loginPage() {
                 if(userNameValue === user.username && userPasswordValue === user.password) {
                     console.log("Du lyckades logga in!");
                     loginPageDiv.style.display = "none";
+                    loginFalse = true;
                 }
-            })
+            }); if(!loginFalse) {
+                alert ("Fel inlogg, testa igen!");
+            }
         })
     })
     
