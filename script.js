d34b52e7453dcc1457f5bad95ad50e3bf569916b
Tuva Gyllensten <tuvagyllensten@gmail.com>
2024-10-11 11:30:28 +0200

    drag and drop

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 import { loginPage } from "./modules/loginpage.mjs"; 
-
+import { DragAndDrop, allowDrop, drop  } from "./modules/drangAndDrop.mjs";
 loginPage();
+
+DragAndDrop(); 
+window.allowDrop = allowDrop;
+window.drop = drop;
