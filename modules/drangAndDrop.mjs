8e17a822cc4753d40946ca363194cb6425c93f03
JehadAbdou <jood111ao@hotmail.com>
2024-10-15 11:43:25 +0200

    editBtn

--- modules/drangAndDrop.mjs
+++ modules/drangAndDrop.mjs
@@ -5,29 +5,37 @@ export function DragAndDrop() {
   let boxInput = document.getElementById("input");
 
   boxInput.setAttribute("type", "text");
-  boxInput.setAttribute("placeholder", "Enter a title");
+  boxInput.setAttribute("placeholder", "Enter a card title");
 
   newCardBtn.addEventListener("click", (e) => {
     if (boxInput.value) {
       let addBox = document.createElement("div");
       let boxName = document.createElement("h3");
-      boxName.innerText = boxInput.value;
+      let editBtn = document.createElement("button");
 
-      addBox.appendChild(boxName);
+      boxName.innerText = boxInput.value;
+      editBtn.innerText = "save";
+      let editInput = document.createElement("input");
+      editInput.setAttribute("type", "text");
+      editInput.setAttribute("placeholder", "Edit card text");
+      addBox.append(boxName, editInput, editBtn);
       boxInput.value = "";
       addBox.className = "kort";
       addBox.draggable = "true";
       addBox.ondragstart = drag;
-
+      editBtn.addEventListener("click", () => {
+        boxName.innerText = editInput.value;
+        editInput.value = "";
+      });
       const randomcolor = Math.floor(Math.random() * 16777215).toString(16);
       addBox.style.backgroundColor = "#" + randomcolor;
 
       addBox.id = "addedBox" + boxcounter;
-      boxcounter++;
 
+      boxcounter++;
       cards.appendChild(addBox);
     } else {
-      alert("write a kort title");
+      alert("write a card title");
     }
   });
 }
