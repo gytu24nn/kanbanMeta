8ac95c58540213362e1fbef4d7ecfa373d448da7
JehadAbdou <jood111ao@hotmail.com>
2024-10-15 10:09:22 +0200

    remved-local=storage

--- modules/localStorag.mjs
+++
@@ -1,9 +0,0 @@
-let data = [];
-export function Save(cardContent) {
-  if (localStorage.getItem("cards") == null) {
-    localStorage.setItem("cards", data);
-  }
-  let oldData = JSON.parse(localStorage.getItem("cards"));
-  oldData.push(cardContent);
-  localStorage.setItem("cards", JSON.stringify(oldData));
-}
